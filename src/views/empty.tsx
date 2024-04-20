3232e4b029dc06c3336bac93991270285e331586
Steve^ <steve@stevepeak.net>
2024-04-19 19:07:28 -0600

    update message

--- src/views/empty.tsx
+++ src/views/empty.tsx
@@ -2,8 +2,8 @@ import { Icon, List } from "@raycast/api";
 
 export const EmptyView = () => (
   <List.EmptyView
-    title="Ask anything!"
-    description={"Type your question or prompt from the search bar and hit the enter key"}
+    title="Ask your inbox anything!"
+    description={"Type your question into the search bar and hit the enter key"}
     icon={Icon.QuestionMark}
   />
 );
